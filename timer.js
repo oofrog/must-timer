acaa351b13ff7c4475962a0cb7083a83ac44c3e9
oofrog <78217274+oofrog@users.noreply.github.com>
2025-02-13 21:13:48 +0900

    타이머 완

--- timer.js
+++ timer.js
@@ -1,48 +1,38 @@
+const mustTimer = document.querySelector(".must-timer");
 const mustForm = document.querySelector(".must-form");
 const mustInput = document.querySelector(".must-form input");
 
-function deleteMust(event) {
-  const div = event.target.parentElement;
-  div.remove();
-}
+let newMustObj = {};
 
-function paintMust(newMustObj) {
-  const mustDiv = document.createElement("div");
-  mustDiv.className = "must-todo";
-  const span = document.createElement("span");
-  span.innerText = newMustObj.text;
-  const button = document.createElement("button");
-  button.innerText = "X";
-  button.addEventListener("click", deleteMust);
-  mustDiv.appendChild(span);
-  mustDiv.appendChild(button);
-  document.body.appendChild(mustDiv);
-}
-
-const mustTimer = document.querySelector(".mustTimer");
-
-function countDown(newMustObj) {
-  const targetTime = newMustObj.targetTime;
+function countDown() {
+  const setTime = newMustObj.time + 1000 * 60 * 60 * 24;
   const now = Date.now();
-  const diff = targetTime - now;
+  const diff = setTime - now;
 
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
-
-  mustTimer.innerText = `${hours}:${minutes}:${seconds}`;
+  const hours = String(Math.floor((diff / (1000 * 60 * 60)) % 24)).padStart(
+    2,
+    "0"
+  );
+  const minutes = String(Math.floor((diff / (1000 * 60)) % 60)).padStart(
+    2,
+    "0"
+  );
+  const seconds = String(Math.floor((diff / 1000) % 60)).padStart(2, "0");
+  if (newMustObj.text !== undefined) {
+    mustTimer.innerText = `${hours}:${minutes}:${seconds}`;
+  }
 }
 
-function handleMustSubmit(event) {
+function handleSubmit(event) {
   event.preventDefault();
   const newMust = mustInput.value;
   mustInput.value = "";
-  const newMustObj = {
+  newMustObj = {
     text: newMust,
     time: Date.now(),
   };
-  paintMust(newMustObj);
-  countDown(newMustObj);
 }
 
-mustForm.addEventListener("submit", handleMustSubmit);
+mustForm.addEventListener("submit", handleSubmit);
+
+setInterval(countDown, 500);
